852847c0cef77bad8840790d35850c3c5b0149a7
Chloe Rice <chloe.rice@shopify.com>
2020-03-07 21:24:57 -0500

    update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Forty V2",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Forty by HTML5 UP"
+    title: 'Gatsby Starter - Forty V2',
+    author: 'Hunter Chang',
+    description: 'A Gatsby.js V2 Starter based on Forty by HTML5 UP',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -12,13 +12,13 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#FED136',
+        theme_color: '#FED136',
         display: 'minimal-ui',
-        icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
+        icon: 'src/assets/images/wbb-square-logo.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-sass',
-    'gatsby-plugin-offline'
+    'gatsby-plugin-offline',
   ],
 }
