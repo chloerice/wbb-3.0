fa7e21ba5682437c1bf25efdf64379de31ec3b60
drerichards <g.andrerichards@gmail.com>
2020-06-01 08:54:25 -0400

    home page sections owners

--- src/components/wrapperComponents/BannerContent.jsx
+++ src/components/wrapperComponents/BannerContent.jsx
@@ -6,10 +6,10 @@ const BannerContent = ({ data }) => {
       {data.map(el => (
         <div key={el.title} className={el.title ? "py-4 lg:w-33pc text-center" : "bg-white"}>
           {el.title === 'Countless' ?
-            <h2 className="text-black-300 text-3r lg:text-3r lg:mb-4 text-yellow-600">{el.title}</h2>
+            <h2 className="text-yellow-600 text-3r lg:text-3r lg:mb-4">{el.title}</h2>
             : <h2 className="text-black-300 text-3r lg:text-3r lg:mb-4">{el.title}</h2>
           }
-          <p className="text-12 text-black-600 uppercase tracking-widest shadowDark">{el.text}</p>
+          <p className="text-12 text-black-600 uppercase tracking-widest font-semibold shadowDark">{el.text}</p>
         </div>
       ))}
     </Fragment>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,13 +3,13 @@ import { Link } from 'gatsby'
 import Helmet from 'react-helmet'
 
 import Layout from '../components/Layout'
-import Banner from '../components/wrapperComponents/Banner'
 
 import BackgroundImage from '../components/wrapperComponents/BackgroundImage'
 import TextContent from '../components/wrapperComponents/TextContent'
 import IconContent from '../components/wrapperComponents/IconContent'
 import BannerContent from '../components/wrapperComponents/BannerContent'
 
+import Image from '../components/elements/Image'
 import Button from '../components/elements/Button'
 
 import woman1 from '../assets/images/woman1.jpeg'
@@ -56,9 +56,8 @@ class HomeIndex extends React.Component {
           </BackgroundImage>
           <div className="w-full bg-yellow-600 flex flex-col">
             <div className="w-full bg-black-900 mt-40 h-40"></div>
-            <BackgroundImage image={texture} addlClasses={'w-p90 -mt-30r self-end'}>
+            <BackgroundImage image={texture} addlClasses={'w-full md:w-p90 -mt-30r self-end'}>
               <div className="w-full">
-
                 <div className="md:flex">
                   <div className="md:flex">
                     <IconContent icon={'laptop'} title={'Our Story'} color={'text-red-400'} />
@@ -74,9 +73,8 @@ class HomeIndex extends React.Component {
                     <BackgroundImage image={texture} />
                   </div>
                 </div>
-
                 <div className="md:flex border-t border-b border-gray-400 border-solid">
-                  <div className="w-full py-4 lg:flex">
+                  <div className="w-full my-8 py-4 lg:flex">
                     <BannerContent data={bannerData} />
                   </div>
                   <div style={{ minWidth: '5vw' }} className="hidden md:block border-l border-gray-400 border-solid">
@@ -87,9 +85,13 @@ class HomeIndex extends React.Component {
                     </div>
                   </div>
                 </div>
-
+                <div className="md:hidden h-full w-full bg-red-400 py-5 flex justify-evenly items-center">
+                  <div className="h-8 w-8 bg-white m-1"></div>
+                  <div className="h-8 w-8 bg-white m-1"></div>
+                  <div className="h-8 w-8 bg-white m-1"></div>
+                </div>
                 <div className="md:flex md:flex-row-reverse">
-                  <div style={{ minWidth: '5vw' }} className="hidden md:block border-l border-gray-400 border-solid">
+                  <div style={{ minWidth: '5vw' }} className="py-8 hidden md:block border-l border-gray-400 border-solid">
                     <BackgroundImage image={texture} />
                   </div>
                   <div>
@@ -110,6 +112,55 @@ class HomeIndex extends React.Component {
                     </div>
                   </div>
                 </div>
+                <div className="flex">
+                  <div className="md:-ml-10r md:grid grid-cols-2 gap-4">
+                    <div className="pt-6r pb-4 md:py-6r -mx-2 md:mx-0">
+                      <div className="w-full">
+                        <Image image={woman9} width={'100%'} height={'70%'} />
+                      </div>
+                    </div>
+                    <div className="md:py-6r">
+                      <div className="max-w-xs mx-auto mb-3r md:mx-8">
+                        <Svg icon='laptop' />
+                      </div>
+                      <div className="mx-8 md:mr-5pc">
+                        <h2 className="text-yellow-600 text-center md:text-left lg:mb-4">Joe Rigelsky</h2>
+                        <p className="mt-4 text-14 text-center md:text-left text-black-900 uppercase tracking-widest font-semibold shadowDark">UPSTREAM FOUNDER</p>
+                        <p className="mt-8 text-black-900 shadowDeep">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima eligendi ipsam porro atque impedit eaque, consectetur velit perspiciatis voluptatem illum placeat aperiam eius laboriosam rem ipsum. Cumque ratione ab repellendus?</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div style={{ minWidth: '5vw' }} className="py-8 hidden md:block border-l border-gray-400 border-solid">
+                    <BackgroundImage image={texture} />
+                  </div>
+                </div>
+                <div className="flex my-6r">
+                  <div className="flex flex-col-reverse md:block md:grid grid-cols-2 gap-4">
+                    <div className="md:py-6r">
+                      <div className="max-w-xs mx-auto mb-3r md:mx-8">
+                        <Svg icon='laptop' />
+                      </div>
+                      <div className="mx-8 md:mr-5pc">
+                        <h2 className="text-yellow-600 text-center md:text-right lg:mb-4">Joe Rigelsky</h2>
+                        <p className="mt-4 text-14 text-center md:text-right text-black-900 uppercase tracking-widest font-semibold shadowDark">UPSTREAM FOUNDER</p>
+                        <p className="mt-8 text-black-900 md:text-right shadowDeep">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae vel animi totam voluptatum minus maiores? Minima eum quia eaque nisi enim provident corrupti quam doloribus amet ducimus dolorem, adipisci voluptatum?</p>
+                      </div>
+                    </div>
+                    <div className="pt-2 pb-4 md:py-6r -mx-2 md:mx-0">
+                      <div className="relative w-full">
+                        <span className="md:absolute z-10">
+                          <Image image={woman9} width={'100%'} height={'100%'} />
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                  <div style={{ minWidth: '5vw' }} className="py-8 hidden md:block border-l border-gray-400 border-solid">
+                    <BackgroundImage image={texture} />
+                  </div>
+                </div>
+                <BackgroundImage image={woman1} color={'black'} addlClasses={'px-5pc py-25pc lg:px-20pc lg:py-15pc'}>
+                  <h1 className="pb-56 text-center text-yellow-600 shadowLight">To break the cycle of poverty, we must embrace the will to navigate Upstream</h1>
+                </BackgroundImage>
               </div>
             </BackgroundImage>
           </div>
